63fc60e2656c12087c73832f494266230fae64da
Keks <keks@htmlacademy.ru>
2018-10-31 17:34:02 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/script.js
@@ -0,0 +1,6 @@
+var header_button_off = document.querySelector(".header__button");
+var header_button_on_open = document.querySelector(".header__button-open");
+var header_button_on_close = document.querySelector(".header__button-close");
+var modal_link = document.querySelector(".weekly-item__button");
+var modal_close = document.querySelector(".modal__close");
+var modal_open = document.querySelector(".modal__open");
