68d93e88deafc2ddfd21b6ab9f64cd3a114c5beb
Pavel <paulkotelnikov1992@gmail.com>
2018-11-07 16:12:22 +0300

    начало js

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,9 @@
 
+var menu_open = document.querySelectorAll(".header__list-item");
+var menu_close = document.querySelector(".header__list-item-close");
+var header_btn = document.querySelector(".header__button");
+var header_btn_on = document.querySelector(".header__button-open");
+var header_btn_on_close = document.querySelector(".header__button-close");
+var modal_index_open = document.querySelector(".weekly-item__button");
 
+header_btn.classList.toggle("header__button-open");
